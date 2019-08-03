afe4c166b9d9dc9e94b47985bdd0e0d9ebd374b7
wangzongq <853058177@qq.com>
2019-08-03 17:09:02 +0800

    idea整合GitHub测试

--- sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
@@ -6,10 +6,12 @@ import com.tedu.sp01.service.UserService;
 import com.tedu.web.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RefreshScope
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
